0ff0a465fb98a82593e38cb4a91a6dbead2187bd
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-08 18:53:10 +0300

    images loaded

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import App from './App';
+import './index.scss';
+import {BrowserRouter} from "react-router-dom";
+import {MantineProvider} from "@mantine/core";
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <MantineProvider withGlobalStyles>
+                {/*<Provider>*/}
+                    <App/>
+                {/*</Provider>*/}
+            </MantineProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
