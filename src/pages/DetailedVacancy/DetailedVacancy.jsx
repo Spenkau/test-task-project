39d97226e2e6e714e4cfa8ce705b44f60af99058
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-13 08:50:37 +0300

    Created component that contain details about vacancy

--- src/pages/DetailedVacancy/DetailedVacancy.jsx
+++ src/pages/DetailedVacancy/DetailedVacancy.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import Vacancy from "../../components/Vacancy/Vacancy";
-import './detailedvacancy.module.scss';
+import {useLocation} from "react-router-dom";
+import {useGetVacancyQuery} from "../../store/apiSlice";
+
 
 const DetailedVacancy = () => {
+    const {state} = useLocation();
+    const {profession, payment_from, payment_to, type_of_work, town_title, id} = state;
+    const {data, isError} = useGetVacancyQuery(id)
+
+    function generateRichText() {
+        if (isError) return 'Ошибка! Перезагрузите страницу'
+
+        if (data)  {
+            const parser = new DOMParser()
+            const parserResult = parser.parseFromString(data.vacancyRichText, 'text/html');
+            return <div dangerouslySetInnerHTML={{ __html: parserResult.body.innerHTML }} />
+        }
+        else return 'Loading...';
+    }
+
     return (
         <main>
-            <ul>
-                <Vacancy/>
+            <ul className="details">
+                <Vacancy profession={profession} payment_from={payment_from} payment_to={payment_to} type_of_work={type_of_work} town_title={town_title} />
+                <li className="details-content">
+                    {generateRichText()}
+                </li>
             </ul>
         </main>
     );
