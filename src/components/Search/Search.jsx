0ce2fda47b1bf9ed7c526dc2d3a06d84b6c4daa3
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-13 08:54:48 +0300

    created search component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {useGetVacanciesByKeywordQuery} from "../../store/apiSlice";
+
+const Search = ({setRenderJobs}) => {
+    const [keyword, setKeyword] = useState('');
+    const [shouldFetch, setShouldFetch] = useState(false)
+    const searchInputRef = useRef(null);
+    const {data: vacanciesByKeyword} = useGetVacanciesByKeywordQuery(keyword, {
+        skip: shouldFetch === false
+    });
+
+    useEffect(() => {
+        if (vacanciesByKeyword) setRenderJobs(vacanciesByKeyword.objects)
+    }, [vacanciesByKeyword])
+
+    const getVacanciesByKeyword = async (e) => {
+        e.preventDefault();
+        setKeyword(searchInputRef.current.value);
+        setShouldFetch(true);
+        searchInputRef.current.value = '';
+    }
+
+    return (
+        <form className="search" onSubmit={(e) => getVacanciesByKeyword(e)}>
+            <img className="left" src="/images/loop.png" width={16} height={16} alt="loop"/>
+            <input ref={searchInputRef} className="left" placeholder="Введите название вакансии" type="text"/>
+            <button className="btn-blue" type="submit">Поиск</button>
+        </form>
+    );
+};
+
+export default Search;
