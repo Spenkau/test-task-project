e240750dc3973085721a71e30a451ef307e5dec9
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-14 22:27:51 +0300

    useSelector in favorites exist already

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Vacancy from "../../components/Vacancy/Vacancy";
+import {useSelector} from "react-redux";
+import {selectFavorites} from "../../store/favoritesSlice";
+import {useLocation} from "react-router-dom";
 
 const Favorites = () => {
+    const favorites = useSelector(selectFavorites)
+
     return (
         <main>
             <ul className="favorites">
-                <Vacancy />
+                {
+                    favorites.map(item =>
+                        <Vacancy profession={item.profession} payment_from={item.payment_from} payment_to={item.payment_to} type_of_work={item.type_of_work} town_title={item.town_title} id={item.id}/>
+                    )
+                }
             </ul>
         </main>
     );
