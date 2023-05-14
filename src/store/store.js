c104a6b9e7b2031daf4aac78c010cc8c0913713d
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-14 22:25:40 +0300

    favorites reducer added

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {apiSlice} from "./apiSlice";
-
+import favoritesReducer from "./favoritesSlice";
 
 export const store = configureStore({
     reducer: {
-
+        favorites: favoritesReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
 });
-
