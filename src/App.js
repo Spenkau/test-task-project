90cb52107fa16fdd4a0432829af81005a6746987
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-22 00:03:27 +0300

    saving to localstorage added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import Home from "./pages/Home/Home";
-import Favorites from "./pages/Favorites/Favorites";
 import {Route, Routes} from 'react-router-dom'
 import Layout from "./features/Layout/Layout";
 import DetailedVacancy from "./pages/DetailedVacancy/DetailedVacancy";
 import EmptyState from "./components/EmptyState/EmptyState";
+import Favorites from "./pages/Favorites/Favorites";
+import {selectFavorites} from "./store/favoritesSlice";
+import {useSelector} from "react-redux";
 
 function App() {
+    const favorites = useSelector(selectFavorites)
+
+    useEffect(() => {
+        localStorage.setItem('favs', JSON.stringify(favorites))
+    }, [favorites])
 
     return (
         <Layout>
             <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                <Route path="/favorites" element={<Favorites/>}/>
-                <Route path="/empty_page" element={<EmptyState />}/>
+                <Route exact path="/" element={<Home />}/>
+                <Route path="/favorites" element={<Favorites />}/>
+                <Route path="/empty_page" element={<EmptyState/>}/>
                 <Route path="/vacancies/:id" element={<DetailedVacancy/>}/>
             </Routes>
         </Layout>
